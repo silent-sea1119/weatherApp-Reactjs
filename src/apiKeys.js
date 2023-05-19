7b57a15b63540ded65fbc94e75f9c2a96235343b
silent-sea1119 <sandragonzales56t@gmail.com>
2023-05-19 06:10:21 -0700

    chore: change comments

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "{Enter Your API Key Here}",
   base: "https://api.openweathermap.org/data/2.5/",
 };
